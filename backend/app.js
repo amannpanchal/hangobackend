4bc17a28330d06d4ec70fe83bc457df4b63f19ee
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-03-15 10:25:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const cookie = require('cookie-parser')
 app.use(cookie())
 app.use(
